8a9a67d688ec30b0366daf81aed65e1f03e2b730
The-Jay-San <jul.a@free.fr>
2021-03-30 15:05:39 +0200

    Postgre table users

--- database/refresh.php
+++ database/refresh.php
@@ -7,3 +7,25 @@ $dotenv->load();
 
 
 $pdo = new \PDO($_ENV['CONNECT_DB']);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$pdo->exec("
+    DO $$ DECLARE
+        r RECORD;
+    BEGIN
+        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+            EXECUTE 'DROP TABLE IF EXISTES ' || quote_ident(r.tablename) || ' CASCADE';
+        END LOOP;
+    END $$;
+")
+
+
+$pdo->exec('CREATE TABLE users (
+    id SERIAL,
+    email TEXT,
+    firstname TEXT,
+    lastname TEXT,
+    adresse TEXT,
+    numbmembers INT,
+    password TEXT
+    )');
